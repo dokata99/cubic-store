1fbc77f87f0b109bc7311d10a3418aadc17df801
dokata99 <72043010+dokata99@users.noreply.github.com>
2021-03-18 18:59:40 +0200

    main layout created

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,10 @@ const router = Router()
 
 
 router.get('/', (req, res) => {
-    res.render('home', { layout: false })
+    res.render('home')
 })
 router.get('/create', (req, res) => {
-    res.render('create', { layout: false })
+    res.render('create')
 })
 
 module.exports = router
