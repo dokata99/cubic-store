620ca2ea8a31134d3e9328c1813fc12a9679235d
dokata99 <72043010+dokata99@users.noreply.github.com>
2021-03-18 18:46:32 +0200

    page 404

--- routes.js
+++ routes.js
@@ -6,6 +6,9 @@ const aboutController = require('./controllers/aboutController')
 
 router.use('/',productController)
 router.use('/about',aboutController)
+router.get('*',(req,res)=>{
+    res.render('404')
+})
 
 
 module.exports = router
