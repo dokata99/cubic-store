7ce113a2c418daac380cbc65818bb817abc0253b
dokata99 <72043010+dokata99@users.noreply.github.com>
2021-03-19 16:57:20 +0200

    save new products in products file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,26 @@
 const { Router } = require('express')
+const productServices = require('../services/productServices')
 const router = Router()
 
 
 router.get('/', (req, res) => {
-    res.render('home',{title: 'Products'})
+    res.render('home', { title: 'Products' })
 })
 router.get('/create', (req, res) => {
-    res.render('create',{title: 'Create'})
+    res.render('create', { title: 'Create' })
 })
-router.post('/create',(req,res)=>{
+router.post('/create', (req, res) => {
     console.log(req.body)
+
+    //TODO: VALIDATE INPUTS!    
+    productServices.create(req.body)
+   
     res.redirect('/')
 
 })
 
-router.get('/details/:productId', (req,res) =>{
-    res.render('details',{title:'Product Details'})
+router.get('/details/:productId', (req, res) => {
+    res.render('details', { title: 'Product Details' })
 })
 
 module.exports = router
---
+++ services/productServices.js
@@ -0,0 +1,32 @@
+const Cube = require('../models/Cube')
+const uniqid = require('uniqid')
+const productsDate = require('../config/products.json')
+const fs = require('fs')
+
+
+function getAll(){
+    return productsDate
+}
+function create(data){
+
+    let cube = new Cube(
+        uniqid(),
+        data.name,
+        data.description,
+        data.imageUrl,
+        data.difficultyLevel
+    )
+    
+    productsDate.push(cube)
+    fs.writeFile(__dirname +'/../config/products.json',JSON.stringify(productsDate), (err) =>{
+        if(err){
+            console.log(err)
+            return
+        }
+    })
+
+}
+
+module.exports = {
+    create
+}
