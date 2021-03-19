67c7490aaeaa28a003858dd1f70c57a4705806ca
dokata99 <72043010+dokata99@users.noreply.github.com>
2021-03-19 15:31:44 +0200

    add details page for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,10 +3,13 @@ const router = Router()
 
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home',{title: 'Products'})
 })
 router.get('/create', (req, res) => {
-    res.render('create')
+    res.render('create',{title: 'Create'})
+})
+router.get('/details/:productId', (req,res) =>{
+    res.render('details',{title:'Product Details'})
 })
 
 module.exports = router
--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ const aboutController = require('./controllers/aboutController')
 router.use('/',productController)
 router.use('/about',aboutController)
 router.get('*',(req,res)=>{
-    res.render('404')
+    res.render('404',{title:"Page Not Found 404"})
 })
 
 
